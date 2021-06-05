7231caf221d7b4bad4cc4c602c38fe5cdea09be6
Cort Smith <cortdsmith@protonmail.com>
2021-06-05 15:43:10 -0700

    Added a new class file about-page and a related css file.

---
+++ src/components/about-page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './about-page.css';
+
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                <div id="comment" name="Return the page jsx for the About page."></div>
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -4,9 +4,43 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Home from './components/home-page';
+
+class Root extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentpage: <Home />
+    };
+  }
+
+  componentWillMount() {}
+  componentWillUnmount() {}
+
+  setCurrentPage(props) {
+    this.setState({currentpage: props});
+  }
+
+  render() {
+    return (
+      <div id="root">
+        <div id="nav-bar">
+          <div id="comment" name="This is a navigation bar that will host all the buttons to move between pages."></div>
+          <div id="comment" name="The nav bar will remain static throughout the pages of the webapp."></div>
+          <button onClick={this.setCurrentPage(<Home />)}>Home</button>
+        </div>
+        <div id="comment" name="Below will be the remaining page that will update when the buttons are pressed."></div>
+        {this.state.currentpage}
+      </div>
+    );
+  }
+}
+
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
