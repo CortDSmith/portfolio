b642614fa9fd62d278d0b8f0e97deb52f9af639b
Cort Smith <cortdsmith@protonmail.com>
2021-06-12 12:50:46 -0700

    Added 3 social media logo image files. Updated the link buttons class identifier in the header and adjusted the attributed css file to match. Added social media links to the header and removed the logo temporarily (haven't decided if I want to fully remove the main logo). Changed the intro text and added a word wrapper to the attributed css.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Header from './Page';
+
+const Text = (props) => {
+  const text = props.text;
+  const textsplit = text.split('\n');
+  const textmap = textsplit.map(str => <p>{str}</p>);
+  return (textmap);
+}
 
 class Root extends React.Component {
   constructor(props) {
@@ -17,46 +25,41 @@ class Root extends React.Component {
   render() {
     return (
       <div>
-        <head>
-          <meta charset="UTF-8"/>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-          <title> Website Layout | CodingLab</title>
-          <link rel="stylesheet" href="style.css"/>
-        </head>
         <body>
           <nav>
             <div class="menu">
               <div class="logo">
-                <a href="#">My Portfolio Test Logo</a>
+                <ul>
+                  <a href="#"></a>
+                  <ul class="social-media">
+                    <li><a href="https://github.com/CortDSmith" name="Github">
+                        <img class="github"></img>
+                    </a></li>
+                    <li><a href="https://www.linkedin.com/in/cort-smith-3a833915a/" name="LinkedIn">
+                        <img class="linkedin"></img>
+                    </a></li>
+                    <li><a href="#" name="">
+                      <img class="twitter"/>
+                    </a></li>
+                  </ul>
+                </ul>
               </div>
               <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">Contact</a></li>
-                <li><a href="#">Feedback</a></li>
+                <li class="tabs"><a href="#">Home</a></li>
+                <li class="tabs"><a href="#">About</a></li>
+                <li class="tabs"><a href="#">Projects</a></li>
+                <li class="tabs"><a href="#">Contact</a></li>
+                <li class="tabs"><a href="#">Feedback</a></li>
               </ul>
             </div>
           </nav>
           <div class="img"></div>
-          <div class="social-media">
-            <ul>
-              <li><a href="#" name="LinkedIn"></a></li>
-              <li><a href="#" name="Github"></a></li>
-              <li><a href="#" name=""></a></li>
-            </ul>
-          </div>
           <div class="center">
             <div class="title">Cort Smith</div>
             <div class="sub_title">Software & Game Developer</div>
-            <div class="quote">"Give everything or nothing. There is no inbetween."</div>
-            <div class="intro">Aspiring software and game developer.</div>
-            <div class="intro-cont">I've applied my knowledge on contract with Ricoh and the Gaming industry</div>
-            <div class="intro-cont">within my coworkers game studio.</div>
-            
-            <div class="btns">
-              <button>Learn More</button>
-              <button>Subscribe</button>
+            <div class="quote">"Give everything or give nothing. There is no inbetween."</div>
+            <div class="intro">
+              Motivated software/game developer looking for opportunities where my skills can contribute to the success of the team. Versed in many technologies around 3D experience and a novice ML engineer.
             </div>
           </div>
         </body>
